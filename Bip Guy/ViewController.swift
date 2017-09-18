f982af95da88a32bfc4b165155522be338c4d295
Michelle Krameisen <michellekrameisen@michelles-mbp-2.bc.edu>
2017-09-18 19:41:07 -0400

    video 4.1

--- Bip Guy/ViewController.swift
+++ Bip Guy/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageToPunch: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func libraryPressed(_ sender: UIButton) {
     }
-
-
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        print("Hey! You Just Pressed the Image!")
+    }
 }
 
